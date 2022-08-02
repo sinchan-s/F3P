09a112ff31938eac947c2d6101b1315599c97683
Sinchan <sinchan.tex@gmail.com>
2022-08-02 23:25:43 +0530

    selectors added

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import streamlit as st
 df = pd.read_csv("Physical data with coverage.csv")
 st.header("Fabric Physical Parameters Predictor")
 
-articles = df['Article No.'].unique()
-s_article = st.selectbox("Select Article", articles)
+total_articles = df['Article No.'].unique()
+s_article = st.selectbox("Select Article", total_articles)
+article_df = df[df['Article No.']==s_article]
 
-st.dataframe(data=df[df['Article No.']==s_article])
+col1, col2, col3 = st.columns(3)
+article = col1.metric('', article_df.Weave.unique()[0])
+finish = col2.selectbox("Finish Code", article_df.Finish.unique())
+style = col3.selectbox("Style", article_df.Style.unique())
+result_df = st.dataframe(article_df[['Warp Shrinkage','Weft Shrinkage', 'Warp Tear','Weft Tear', 'Warp Tensile', 'Weft Tensile','Warp Slippage', 'Weft Slippage', 'Growth', 'Elongation', 'GSM']])
+
+#st.dataframe(data=article_df)
