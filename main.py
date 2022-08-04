44c3c2562c53d329c912e21d8c44450dfc8cc507
Sinchan <sinchan.tex@gmail.com>
2022-08-05 00:16:18 +0530

    fixed count parameter

--- main.py
+++ main.py
@@ -4,22 +4,36 @@ import streamlit as st
 # reading the source file
 df = pd.read_csv("Physical data with coverage.csv")
 
+
 # an apt heading
 st.header("Fabric Physical Parameters Predictor")
+
 
 # article selection
 all_articles = df['Article No.'].unique()
 select_article = st.selectbox("Select Article", all_articles)
 article_df = df[df['Article No.']==select_article]
 
+
+# warp-weft count segregation and identification
+#count = article_df['Warp*Weft'][0].split('*')
+#warp_count = count[0]
+#weft_count = count[1]
+count = article_df['Warp*Weft']
+
+# column-wise display & selection
 col1, col2, col3 = st.columns(3)
 article = col1.metric('Weave', article_df.Weave.unique()[0])
-count = col1.metric('Count',article_df['Warp*Weft'][0])
+#count_warp = col1.metric('Warp Count',warp_count)
+#count_weft = col1.metric('Weft Count',weft_count)
+#count_const = col1.metric('Const', count)
 finish = col2.selectbox("Select Finish Code", article_df.Finish.unique())
 style = col2.selectbox("Select Style", article_df.Style.unique())
+
+
+# final display dataframe
 result_df = df.loc[(df['Article No.']==select_article)&(df['Finish']==finish)&(df['Style']==style)]
-result_df_display = st.dataframe(result_df[['EPI','PPI', 'F_Width', 'Warp Shrinkage','Weft Shrinkage', 'Warp Tear','Weft Tear', 'Warp Tensile', 'Weft Tensile','Warp Slippage', 'Weft Slippage', 'Growth', 'Elongation', 'GSM']])
+result_df_display = st.dataframe(result_df[['Warp*Weft', 'EPI','PPI', 'Finish Width', 'Coverage group', 'Warp Shrinkage','Weft Shrinkage', 'Warp Tear','Weft Tear', 'Warp Tensile', 'Weft Tensile','Warp Slippage', 'Weft Slippage', 'Growth', 'Elongation', 'GSM']])
 
 
-
-#st.dataframe(data=result_df)
+#st.dataframe(data=count)
