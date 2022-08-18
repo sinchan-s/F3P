2b4201bd5d3918d751c485dd8ea625fcd8fce47c
Sinchan <sinchan.tex@gmail.com>
2022-08-18 23:49:34 +0530

    more graphs

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import streamlit as st
+import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
+
+
+sns.set_style('whitegrid')
 
 # reading the source file
 df = pd.read_csv("Physical data with coverage.csv")
@@ -37,9 +41,20 @@ style = col3.selectbox("Select Style", article_df.Style.unique())
 result_df = df.loc[(df['Article No.']==select_article)&(df['Finish']==finish)&(df['Style']==style)]
 result_df_display = st.dataframe(result_df[['Warp*Weft', 'EPI','PPI', 'Finish Width', 'Coverage group', 'Warp Shrinkage','Weft Shrinkage', 'Warp Tear','Weft Tear', 'Warp Tensile', 'Weft Tensile','Warp Slippage', 'Weft Slippage', 'Growth', 'Elongation', 'GSM']])
 
-fig1 = px.scatter(result_df, x='EPI', y='PPI')
-st.plotly_chart(fig1)
+# graphical section
+col1, col2 = st.columns(2)
+fig1 = plt.figure(figsize=(8, 3))
+sns.boxplot(data=result_df, x='EPI')
+col1.pyplot(fig1)
 
-fig2 = px.scatter(result_df, x='Warp Tensile', y='Weft Tensile')
-st.plotly_chart(fig2)
-#st.dataframe(data=count)
+fig2 = plt.figure(figsize=(8, 3))
+sns.boxplot(data=result_df, x='PPI')
+col2.pyplot(fig2)
+
+fig3 = plt.figure(figsize=(8, 3))
+sns.histplot(data=result_df, x='Warp Tensile', y='Weft Tensile', hue='Coverage group')
+col1.pyplot(fig3)
+
+fig4 = plt.figure(figsize=(8, 3))
+sns.histplot(data=result_df, x='Warp Tear', y='Weft Tear', hue='Coverage group')
+col2.pyplot(fig4)
