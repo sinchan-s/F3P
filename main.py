1bc0fead35369d1790e369821a51072bf03417e9
Sinchan <sinchan.tex@gmail.com>
2022-09-03 23:04:00 +0530

    coverage df & column added

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ sns.set_style('darkgrid')
 
 # reading the source file
 df = pd.read_csv("Physical data with coverage2.csv")
+cover_df = pd.read_csv("Coverage data.csv")
 
 # an apt heading
 st.header("Fabric Physical Parameters Predictor")
@@ -34,39 +35,41 @@ finish = col2.selectbox("Select Finish Code", article_df.Finish.unique())
 style = col3.selectbox("Select Style", article_df.Style.unique())
 
 # df is display
-result_df = df.loc[(df['Article No.']==select_article)&(df['Finish']==finish)&(df['Style']==style)]
+selection_df = df.loc[(df['Article No.']==select_article)&(df['Finish']==finish)&(df['Style']==style)]
+merged_df = selection_df.merge(cover_df, how='left', left_on=['Print Design', 'Print Color'], right_on=['DESIGN NUMBER', 'Colour Code'])
 
 # graphical sections divided into 2 columns
 st.subheader(f'Tear Strength Parameters')
 col1, col2 = st.columns(2)
 fig1 = plt.figure(figsize=(8, 3))
-weft_tear = sns.boxplot(data=result_df, x='Weft Tear', y='Coverage group', width=0.5, linewidth=2, whis=1, palette='rocket').set(title='Weft Tear range')
+weft_tear = sns.boxplot(data=selection_df, x='Weft Tear', y='Coverage group', width=0.5, linewidth=2, whis=1, palette='rocket').set(title='Weft Tear range')
 col1.pyplot(fig1)
 
 fig2 = plt.figure(figsize=(8, 3))
-warp_tear = sns.boxplot(data=result_df, x='Warp Tear', y='Coverage group', width=0.5, linewidth=2, whis=1, palette='winter').set(title='Warp Tear range')
+warp_tear = sns.boxplot(data=selection_df, x='Warp Tear', y='Coverage group', width=0.5, linewidth=2, whis=1, palette='winter').set(title='Warp Tear range')
 col2.pyplot(fig2)
 
 st.subheader(f'Tensile Strength Parameters')
 col1, col2 = st.columns(2)
 fig3 = plt.figure(figsize=(8, 3))
-weft_tens = sns.boxplot(data=result_df, y='Weft Tensile', x='Coverage group', width=0.5, linewidth=2, palette='rocket', whis=1).set(title='Weft Tensile range')
+weft_tens = sns.boxplot(data=selection_df, y='Weft Tensile', x='Coverage group', width=0.5, linewidth=2, palette='rocket', whis=1).set(title='Weft Tensile range')
 col1.pyplot(fig3)
 
 fig4 = plt.figure(figsize=(8, 3))
-warp_tens = sns.boxplot(data=result_df, y='Warp Tensile', x='Coverage group', width=0.5, linewidth=2, whis=1, palette='winter').set(title='Warp Tensile range')
+warp_tens = sns.boxplot(data=selection_df, y='Warp Tensile', x='Coverage group', width=0.5, linewidth=2, whis=1, palette='winter').set(title='Warp Tensile range')
 col2.pyplot(fig4)
 
 st.subheader(f'Stretch Parameters')
 col1, col2 = st.columns(2)
 fig5 = plt.figure(figsize=(8, 3))
-growth = sns.scatterplot(data=result_df, x='Growth', y='Coverage group', hue='Coverage group', palette='rocket').set(title='Growth range')
+growth = sns.scatterplot(data=selection_df, x='Growth', y='Coverage group', hue='Coverage group', palette='rocket').set(title='Growth range')
 col1.pyplot(fig5)
 
 fig6 = plt.figure(figsize=(8, 3))
-elong = sns.scatterplot(data=result_df, x='Elongation', y='Coverage group', hue='Coverage group', palette='winter').set(title='Elongation range')
+elong = sns.scatterplot(data=selection_df, x='Elongation', y='Coverage group', hue='Coverage group', palette='winter').set(title='Elongation range')
 col2.pyplot(fig6)
 
 # dataframe display
 st.subheader(f'Article dataframe: {select_article}')
-result_df_display = st.dataframe(result_df[['Warp*Weft', 'EPI','PPI', 'Finish Width', 'Coverage group', 'Warp Shrinkage','Weft Shrinkage', 'Warp Tear','Weft Tear', 'Warp Tensile', 'Weft Tensile','Warp Slippage', 'Weft Slippage', 'Growth', 'Elongation', 'GSM' ]])
+#selection_df_display = st.dataframe(selection_df[[ 'EPI','PPI', 'Finish Width', 'Coverage group', 'Warp Shrinkage','Weft Shrinkage', 'Warp Tear','Weft Tear', 'Warp Tensile', 'Weft Tensile','Warp Slippage', 'Weft Slippage', 'Growth', 'Elongation', 'GSM', 'Warp*Weft']])
+merged_df_display = st.dataframe(merged_df[[ 'EPI','PPI', 'Finish Width', 'Total coverage', 'Coverage group', 'Warp Shrinkage','Weft Shrinkage', 'Warp Tear','Weft Tear', 'Warp Tensile', 'Weft Tensile','Warp Slippage', 'Weft Slippage', 'Growth', 'Elongation', 'GSM', 'Warp*Weft']])
