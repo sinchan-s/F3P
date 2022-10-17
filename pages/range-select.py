79f92e6d119bf061186e6907a72ef16b8610bc3e
Sinchan <sinchan.tex@gmail.com>
2022-10-17 23:53:47 +0530

    fine tune description

--- pages/range-select.py
+++ pages/range-select.py
@@ -3,18 +3,18 @@ import pandas as pd
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
+import re
 
 # basic configurations
 st.set_page_config(
     page_title="F3P Main : Range Select",       # similar to <title> tag
     page_icon="🔍",                             # page icon
     layout="wide",                              # widen-out view of the layout
-    initial_sidebar_state="collapsed",          # side-bar state when page-load
-)
+    initial_sidebar_state="collapsed")          # side-bar state when page-load
+
 
 # seaborn graph styling
 sns.set_style('darkgrid')
-
 
 # reading the source file
 df = pd.read_csv("main_data.csv")
@@ -24,20 +24,23 @@ articles_df = pd.read_csv("articles.csv")
 # an apt heading
 st.header("Fabric Physical Parameters Predictor: Range-Select")
 
-col1, col2, col3, col4, col5 = st.columns(5)
+# single column for single article select
+all_articles = articles_df['K1'].unique()
+article_select = st.selectbox("Articles", all_articles)
+selection_df = articles_df.loc[(articles_df['K1']==article_select)]
+selection_df['warp'], selection_df['weft'] = selection_df['Warp*Weft'].str.split("*",1).str
 
 # more finer selection
-df['warp'], df['weft'] = df['Warp*Weft'].str.split("*",1).str
-all_warp_list = df['warp'].unique()
+col1, col2, col3, col4, col5 = st.columns(5)
+
+all_warp_list = selection_df['warp'].unique()
 warp_count_select = col1.selectbox("Warp count:", all_warp_list)
 warp_spun_select = col2.selectbox("Warp compo:", all_warp_list)
-all_weft_list = df['weft'].unique()
+all_weft_list = selection_df['weft'].unique()
 weft_count_select = col3.selectbox("Weft count:", all_weft_list)
 weft_spun_select = col4.selectbox("Weft compo:", all_weft_list)
-all_weaves = df['Weave'].unique()
+all_weaves = selection_df['Weave'].unique()
 weave_selectbox = col5.selectbox("Weave select:", all_weaves)
-all_articles = articles_df['K1'].unique()
-article_select = col1.selectbox("Articles", all_articles)
 
-selection_df = articles_df.loc[(articles_df['K1']==article_select)]
+# dataframe display
 df_display = st.table(selection_df)
