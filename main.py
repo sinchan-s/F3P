5d35cfee60c94daf4067a0964121863f5c0ea1b5
Sinchan <sinchan.tex@gmail.com>
2022-10-21 19:56:49 +0530

    colorified comments

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-# important librabries
+
+#! important librabries
 import pandas as pd
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# basic configurations
+#! basic configurations
 st.set_page_config(
     page_title="F3P Main",
     page_icon="📊",
@@ -12,18 +13,18 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
-# seaborn graph styling
+#! seaborn graph styling
 sns.set_style('darkgrid')
 
-# reading the source file
+#! reading the source file
 df = pd.read_csv("main_data.csv")
 cover_df = pd.read_csv("Coverage data.csv")
-#main_df = pd.read_excel("main_params_data.xlsx", sheet_name='Final QA testing Report')
+main_df = pd.read_csv("main_params_data.csv")
 
-# an apt heading
+#! an apt heading
 st.header("Fabric Physical Parameters Predictor")
 
-# column-wise split: article selection & finsih-style selection
+#! column-wise split: article selection & finsih-style selection
 col1, col2, col3 = st.columns(3)
 
 all_articles = df['Article No.'].unique()
@@ -32,7 +33,7 @@ article_df = df[df['Article No.']==article_selectbox]
 finish = col2.selectbox("Select Finish Code:", article_df.Finish.unique())
 style = col3.selectbox("Select Style:", article_df.Style.unique())
 
-# column-wise split: weave display & warp-weft count display
+#! column-wise split: weave display & warp-weft count display
 col1, col2, col3 = st.columns(3)
 weave = article_df.Weave.unique()[0]
 gsm = article_df.GSM.unique()[0]
@@ -42,10 +43,10 @@ tpi_data = article_df['EPI*PPI'].unique()[0].split("*")
 count_both = col2.metric('Warp || Weft', count_data[0]+' || '+count_data[1])
 tpi = col3.metric('EPI || PPI', tpi_data[0]+' || '+tpi_data[1])
 
-# dataframes merger
+#! dataframes merger
 selection_df = df.loc[(df['Article No.']==article_selectbox)&(df['Finish']==finish)&(df['Style']==style)]
 
-# plotting function
+#! plotting function
 def box_plot(df_select, col_num, x, y, palette, title, std):
     fig = plt.figure(figsize=(8, 3))
     value_to_plot = sns.boxplot(data=df_select, x=x, y=y, showcaps=False, width=0.5, linewidth=2, whis=1, palette=palette).set(title=title)
@@ -56,7 +57,7 @@ def box_plot(df_select, col_num, x, y, palette, title, std):
     #fig.plot(x = 13, color = 'b', label = '12')
     col_num.pyplot(fig)
 
-# column-wise split: graphical charts
+#! column-wise split: graphical charts
 st.subheader(f'Tear Strength Parameter plots:')
 col1, col2 = st.columns(2)
 sorted_cover_group = selection_df.sort_values(by=["Coverage group"], ascending=False)
@@ -73,6 +74,6 @@ col1, col2 = st.columns(2)
 growth = box_plot(selection_df, col1, 'Growth', sorted_cover_group['Coverage group'], 'rocket', 'Growth Range', 'Growth')
 elongation = box_plot(selection_df, col2, 'Elongation', sorted_cover_group['Coverage group'], 'winter', 'Elongation Range', 'Elongation')
 
-# article dataframe display
+#! article dataframe display
 st.subheader(f'Article raw data: {article_selectbox}')
 df_display = st.dataframe(selection_df)
--- pages/range-select.py
+++ pages/range-select.py
@@ -1,36 +1,37 @@
-# important librabries
+
+#! important librabries
 import pandas as pd
 import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 import re
 
-# basic configurations
+#! basic configurations
 st.set_page_config(
-    page_title="F3P Main : Range Select",       # similar to <title> tag
-    page_icon="🔍",                             # page icon
-    layout="wide",                              # widen-out view of the layout
-    initial_sidebar_state="collapsed")          # side-bar state when page-load
+    page_title="F3P Main : Range Select",       #! similar to <title> tag
+    page_icon="🔍",                             #! page icon
+    layout="wide",                              #! widen-out view of the layout
+    initial_sidebar_state="collapsed")          #! side-bar state when page-load
 
 
-# seaborn graph styling
+#! seaborn graph styling
 sns.set_style('darkgrid')
 
-# reading the source file
+#! reading the source file
 df = pd.read_csv("main_data.csv")
 cover_df = pd.read_csv("Coverage data.csv")
 articles_df = pd.read_csv("articles.csv")
 
-# an apt heading
+#! an apt heading
 st.header("Fabric Physical Parameters Predictor: Range-Select")
 
-# single column for single article select
+#! single column for single article select
 all_articles = articles_df['K1'].unique()
 article_select = st.selectbox("Articles", all_articles)
 selection_df = articles_df.loc[(articles_df['K1']==article_select)]
 selection_df['warp'], selection_df['weft'] = selection_df['Warp*Weft'].str.split("*",1).str
 
-# more finer selection
+#! more finer selection
 col1, col2, col3, col4, col5 = st.columns(5)
 wa_match = r"\d+"
 all_warp_list = selection_df['warp'].unique()
@@ -43,8 +44,8 @@ weft_spun_select = col4.selectbox("Weft compo:", all_weft_list)
 all_weaves = selection_df['Weave'].unique()
 weave_selectbox = col5.selectbox("Weave select:", all_weaves)
 
-# metrics display
+#! metrics display
 wa_count_display = st.metric('Warp Count',wa_count_search[0])
 wa_disp = st.write(wa_count_search)
-# dataframe display
+#! dataframe display
 df_display = st.table(selection_df)
