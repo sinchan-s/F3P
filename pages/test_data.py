c02e97c59044e34b2a96c8479c50a37d17133d6a
Sinchan <sinchan.tex@gmail.com>
2022-11-03 23:36:06 +0530

    test #6

--- pages/test_data.py
+++ pages/test_data.py
@@ -22,18 +22,24 @@ df = pd.read_csv("main_params_data.csv")
 df['style'] = ['Pigment' if x.startswith('P') or x.startswith('DU') else 'Reactive' if x.startswith('R') else 'Discharge' for x in df['Print Color']]
 
 #! an apt heading
-st.header("experimentation domain")
+st.header("experimentation tab")
 
 #! column-wise split: article selection & finsih-style selection
 col1, col2, col3 = st.columns(3)
 
-all_articles = df['Article No.'].unique()
-article_selectbox = col1.selectbox("Select Article:", all_articles,) #default='14015')
-article_df = df[df['Article No.']==article_selectbox]
-style = col2.radio("Select Style:", article_df['style'].unique())
-finish = col3.multiselect("Select Finish Code:", article_df.Pattern.unique(), default=article_df.Pattern.unique()[0])
-st.write(f"debug : {finish}")
-selection_df = df.loc[(df['Article No.']==article_selectbox)&(df['Pattern'].isin(finish))&(df['style']==style)]
+try:
+    all_articles = df['Article No.'].unique()
+    article_selectbox = col1.multiselect("Select Article:", all_articles, default='14015')
+    article_df = df[df['Article No.'].isin(article_selectbox)]
+    finish = col2.multiselect("Select Finish Code:", article_df.Pattern.unique(), default=article_df.Pattern.unique()[0])
+    style = col3.radio("Select Style:", article_df['style'].unique())
+    st.write(f"debug : {finish}")
+    selection_df = df.loc[(df['Article No.'].isin(article_selectbox))&(df['Pattern'].isin(finish))&(df['style']==style)]
+except:
+    st.subheader("Please select data to display 👆")
 
 #! dataframe display
-df_display = st.dataframe(selection_df)
+try:
+    df_display = st.dataframe(selection_df)
+except:
+    st.subheader("No table to show 😔")
