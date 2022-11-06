b58ebdcf87d50dd6b059d9681cbd12ae3fb6c3a1
Sinchan <sinchan.tex@gmail.com>
2022-11-06 23:25:18 +0530

    test #11

--- pages/test_data.py
+++ pages/test_data.py
@@ -29,20 +29,20 @@ col1, col2, col3 = st.columns(3)
 
 try:
     all_articles = df['Article No.'].unique()
-    article_selectbox = col1.multiselect("Select Article:", all_articles, default='A1600187', help="You can select multiple articles")
+    article_selectbox = col1.multiselect("Select Article:", all_articles, default='A1600187', help="Select one or more articles to see the data table")
     article_df = df[df['Article No.'].isin(article_selectbox)]
-    finish = col2.multiselect("Select Finish Code:", article_df.Pattern.unique(), default=article_df.Pattern.unique()[0], help="You can select multiple finish codes")
+    finish = col2.multiselect("Select Finish Code:", article_df.Pattern.unique(), default=article_df.Pattern.unique()[0], help="Select one or more finish codes to see the data table")
     style = col3.radio("Select Style:", article_df['style'].unique())
     #st.write(f"debug : {finish}")
     selection_df = df.loc[(df['Article No.'].isin(article_selectbox))&(df['Pattern'].isin(finish))&(df['style']==style)]
 except:
-    st.subheader("Please select data to display 👆")
+    st.error("Please select data to display 👆")
 
 #! dataframe display
 tab1, tab2 = st.tabs(["Selected Data", "All Data"])
 try:
     df_display = tab1.dataframe(selection_df)
 except:
-    st.write("No data to show but still you see All Data")
+    st.warning("No data to show but still you see All Data")
 finally:
     df_display = tab2.dataframe(df)
