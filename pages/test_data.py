9b417dd57d0dcd61bf77edeef49811469f8172c2
Sinchan <sinchan.tex@gmail.com>
2022-11-08 00:10:39 +0530

    test #13

--- pages/test_data.py
+++ pages/test_data.py
@@ -17,9 +17,10 @@ sns.set_style('darkgrid')
 
 #! reading the source file
 df = pd.read_csv("main_params_data.csv")
+main_df = df.dropna(subset=['Warp Tear    (ASTMD1424)'])
 
 #! conditional columns
-df['style'] = ['Pigment' if x.startswith('P') or x.startswith('DU') else 'Reactive' if x.startswith('R') else 'Discharge' for x in df['Print Color']]
+main_df['style'] = ['Pigment' if x.startswith('P') or x.startswith('DU') else 'Reactive' if x.startswith('R') else 'Discharge' for x in main_df['Print Color']]
 
 #! an apt heading
 st.header("experimentation tab")
@@ -28,13 +29,13 @@ st.header("experimentation tab")
 col1, col2, col3 = st.columns(3)
 
 try:
-    all_articles = df['Article No.'].unique()
-    article_selectbox = col1.multiselect("Select Article:", all_articles, default='A1600187', help="Select one or more articles to see the data table")
-    article_df = df[df['Article No.'].isin(article_selectbox)]
+    all_articles = main_df['Article No.'].unique()
+    article_selectbox = col1.multiselect("Select Article:", sorted(all_articles), default=all_articles[0], help="Select one or more articles to see the data table")
+    article_df = main_df[main_df['Article No.'].isin(article_selectbox)]
     finish = col2.multiselect("Select Finish Code:", article_df.Pattern.unique(), default=article_df.Pattern.unique()[0], help="Select one or more finish codes to see the data table")
     style = col3.radio("Select Style:", article_df['style'].unique())
     #st.write(f"debug : {finish}")
-    selection_df = df.loc[(df['Article No.'].isin(article_selectbox))&(df['Pattern'].isin(finish))&(df['style']==style)]
+    selection_df = main_df.loc[(main_df['Article No.'].isin(article_selectbox))&(main_df['Pattern'].isin(finish))&(main_df['style']==style)]
 except:
     st.error("Please select data to display 👆")
 
@@ -46,7 +47,7 @@ try:
 except:
     st.warning("No data to show but still you see All Data")
 finally:
-    df_display = tab2.dataframe(df)
+    df_display = tab2.dataframe(main_df)
 
-st.file_uploader("Upload latest file", type=['csv','xlsx'], accept_multiple_files=False, help="Only upload Article-Wise param data file")
+st.file_uploader("Upload latest data", type=['csv','xlsx'], accept_multiple_files=False, help="Only upload Article-Wise param data file")
 
