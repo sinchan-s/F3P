c8ae57b5be8a6fa457d17ebaaaa43ee0bff88eb9
Sinchan <sinchan.tex@gmail.com>
2023-01-20 23:25:59 +0530

    test #16.4

--- pages/test_data.py
+++ pages/test_data.py
@@ -1,5 +1,4 @@
-
-#! important librabries
+# ! important librabries
 import re
 import pandas as pd
 import streamlit as st
@@ -7,42 +6,47 @@ import seaborn as sns
 from io import StringIO
 import matplotlib.pyplot as plt
 
-#! basic configurations
+# ! basic configurations
 st.set_page_config(
     page_title="testing domain",
     page_icon="❓",
     layout="wide",
     initial_sidebar_state="collapsed")
 
-#! seaborn graph styling
+# ! seaborn graph styling
 sns.set_style('darkgrid')
 
-#! reading the source file
+# ! reading the source file
 df = pd.read_csv("main_params_data.csv")
 main_df = df.dropna(subset=['Warp Tear    (ASTMD1424)'])
 
-#! conditional columns
-main_df['style'] = ['Pigment' if x.startswith('P') or x.startswith('DU') else 'Reactive' if x.startswith('R') else 'Discharge' for x in main_df['Print Color']]
+# ! conditional columns
+main_df['style'] = [
+    'Pigment' if x.startswith('P') or x.startswith('DU') else 'Reactive' if x.startswith('R') else 'Discharge' for x in
+    main_df['Print Color']]
 
-#! an apt heading
+# ! an apt heading
 st.header("experimentation tab")
 
-#! column-wise split: article selection & finsih-style selection
+# ! column-wise split: article selection & finsih-style selection
 col1, col2, col3 = st.columns(3)
 
 try:
     all_articles = main_df['Article No.'].unique()
-    article_selectbox = col1.multiselect("Select Article:", sorted(all_articles), default=all_articles[0], help="Choose one or more articles to see the data table")
+    article_selectbox = col1.multiselect("Select Article:", sorted(all_articles), default=all_articles[0],
+                                         help="Choose one or more articles to see the data table")
     article_df = main_df[main_df['Article No.'].isin(article_selectbox)]
-    finish = col2.multiselect("Select Finish Code:", article_df.Pattern.unique(), default=article_df.Pattern.unique()[0], help="Choose one or more finish codes to see the data table")
+    finish = col2.multiselect("Select Finish Code:", article_df.Pattern.unique(),
+                              default=article_df.Pattern.unique()[0],
+                              help="Choose one or more finish codes to see the data table")
     style = col3.radio("Select Style:", article_df['style'].unique(), help="Choose any print style")
-    #st.write(f"debug : {finish}")
-    selection_df = main_df.loc[(main_df['Article No.'].isin(article_selectbox))&(main_df['Pattern'].isin(finish))&(main_df['style']==style)]
+    # st.write(f"debug : {finish}")
+    selection_df = main_df.loc[(main_df['Article No.'].isin(article_selectbox)) & (main_df['Pattern'].isin(finish)) & (
+            main_df['style'] == style)]
 except:
     st.error("Please select data to display 👆")
 
-
-#! dataframe display
+# ! dataframe display
 with st.expander("Data Tables", expanded=False):
     tab1, tab2 = st.tabs(["Selected Data", "All Data"])
     try:
@@ -52,7 +56,6 @@ with st.expander("Data Tables", expanded=False):
     finally:
         df_display = tab2.dataframe(main_df)
 
-
 # QTX file uploader
 qtx_file = st.file_uploader("Upload QTX format file only:", type=['qtx'], accept_multiple_files=False,
                             help="Only upload QTX file")
@@ -61,20 +64,23 @@ qtx_file = st.file_uploader("Upload QTX format file only:", type=['qtx'], accept
 try:
     stringio = StringIO(qtx_file.getvalue().decode("utf-8"))
     string_data = stringio.read()
-    st.write('str_read: ', string_data)
-    z = re.findall("STD_REFLLOW=(\d+),", string_data)
-    a = re.findall("STD_REFLPOINTS=(\d+),", string_data)
-    b = re.findall("STD_REFLINTERVAL=(\d+),", string_data)
-    c = re.findall("STD_R[=,](.+)", string_data)
-    #st.write(z, a, b, c)
-    #sd_df = pd.DataFrame()
-    for i, j in enumerate(z):
-        ref_low, ref_pts, ref_intv = int(z[i]), int(a[i]), int(b[i])
-        ref_val_list = str(c[i]).split(',')
-        wave_list = [k for k in range(ref_low, ref_low + ref_pts * ref_intv, ref_intv)]
-        sd_df = pd.DataFrame(ref_val_list, index=wave_list, columns=['Spectral_waveform'])
-        sd_df['Spectral_waveform'] = sd_df['Spectral_waveform'].astype('float64')
-        st.line_chart(sd_df)
-        st.dataframe(sd_df)
+    with st.expander('Raw data: ', expanded=False):
+        st.write(string_data)
+    std_name = re.findall("STD_NAME=(.+)", string_data)
+    name_select = st.selectbox("Select Color", std_name)
+    std_i = std_name.index(name_select)
+    list_ref_low = re.findall("STD_REFLLOW=(\d+),", string_data)
+    list_ref_pts = re.findall("STD_REFLPOINTS=(\d+),", string_data)
+    list_ref_int = re.findall("STD_REFLINTERVAL=(\d+),", string_data)
+    list_ref_vals = re.findall("STD_R[=,](.+)", string_data)
+    col1, col2 = st.columns(2)
+    ref_low, ref_pts, ref_int = int(list_ref_low[std_i]), int(list_ref_pts[std_i]), int(list_ref_int[std_i])
+    ref_max = ref_low + ref_pts * ref_int
+    y_ref_val_list = str(list_ref_vals[std_i]).split(',')
+    x_wave_list = [k for k in range(ref_low, ref_max, ref_int)]
+    sd_df = pd.DataFrame(y_ref_val_list, index=x_wave_list, columns=[name_select])
+    sd_df[name_select] = sd_df[name_select].astype('float64')
+    #col1.write(f'{ref_low=}, {ref_pts=}, {ref_int=}, {ref_max=}')
+    st.line_chart(sd_df)
 except:
-    st.write("Unable to preview file !!")
+    st.write("Upload a file !")
