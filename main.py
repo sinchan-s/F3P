7fce1b90c764864a488b6201398115ed4280c82f
Sinchan <sinchan.tex@gmail.com>
2023-04-07 22:51:13 +0530

    code refactored #1

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ st.set_page_config(
     initial_sidebar_state="collapsed",
 )
 
+#! Clean Footer
+hide_default_format = """
+       <style>
+       #MainMenu {visibility: hidden;}
+       footer {visibility: hidden;}
+       </style>
+       """
+st.markdown(hide_default_format, unsafe_allow_html=True)
+
 #! seaborn graph styling
 sns.set_style('darkgrid')
 
@@ -27,27 +36,33 @@ st.header("Fabric Physical Parameters Predictor")
 col1, col2, col3 = st.columns(3)
 
 all_articles = df['Article No.'].unique()
-article_selectbox = col1.selectbox("Select Article:", all_articles)
+with col1:
+    article_selectbox = st.selectbox("Select Article:", all_articles)
 article_df = df[df['Article No.']==article_selectbox]
-finish = col2.multiselect("Select Finish Code:", article_df.Finish.unique(), default=article_df.Finish.unique()[0], help="Select one or more finish codes to see the data table")
-style = col3.radio("Select Style:", article_df.Style.unique())
+with col2:
+    finish = st.multiselect("Select Finish Code:", article_df.Finish.unique(), default=article_df.Finish.unique()[0], help="Select one or more finish codes to see the data table")
+with col3:
+    style = st.radio("Select Style:", article_df.Style.unique())
 
 
 #! column-wise split: weave display & warp-weft count display
 col1, col2, col3 = st.columns(3)
 weave = article_df.Weave.unique()[0]
 gsm = article_df.GSM.unique()[0]
-article = col1.metric('Weave || GSM', weave+' || '+str(gsm))
-count_data = article_df['Warp*Weft'].unique()[0].split("*")         #!split into a list 
-tpi_data = article_df['EPI*PPI'].unique()[0].split("*")
-count_both = col2.metric('Warp || Weft', count_data[0]+' || '+count_data[1])
-tpi = col3.metric('EPI || PPI', tpi_data[0]+' || '+tpi_data[1])
+count_data = article_df['Warp*Weft'].unique()[0].split("*")         #? splits into a list of warp & weft count
+tpi_data = article_df['EPI*PPI'].unique()[0].split("*")             #? splits into a list of EPI & PPI
+with col1:
+    st.metric('Weave || GSM', weave+' || '+str(gsm))                #? diplay weave & gsm
+with col2:
+    st.metric('Warp || Weft', count_data[0]+' || '+count_data[1])   #? display warp-weft count
+with col3:
+    st.metric('EPI || PPI', tpi_data[0]+' || '+tpi_data[1])         #? display epi & ppi
 
 #! dataframes merger
 selection_df = df.loc[(df['Article No.']==article_selectbox)&(df['Finish'].isin(finish))&(df['Style']==style)]
 
 #! plotting function
-def box_plot(df_select, col_num, x, y, palette, title, std):
+def box_plot(df_select, x, y, palette, title, std):
     fig = plt.figure(figsize=(8, 3))
     value_to_plot = sns.boxplot(data=df_select, x=x, y=y, showcaps=False, width=0.5, linewidth=2, whis=1, palette=palette).set(title=title)
     mean_val = df_select[x].mean()
@@ -55,32 +70,40 @@ def box_plot(df_select, col_num, x, y, palette, title, std):
     plt.axvline(mean_val, c='m', ls='--', lw=3)
     plt.axvline(std_mean_val, c='r', ls='-', lw=1)
     #*fig.plot(x = 13, color = 'b', label = '12')
-    col_num.pyplot(fig)
+    st.pyplot(fig)
 
 #! tabular construct
 tab1, tab2 = st.tabs(["Parameter Plots", "Article Tabular Data"])
     
 #! column-wise split: graphical charts
 try:
-    tab1.subheader(f'Tear Strength')
-    col1, col2 = tab1.columns(2)
-    sorted_cover_group = selection_df.sort_values(by=["Coverage group"], ascending=False)
-    weft_tear = box_plot(selection_df, col2, 'Weft Tear', sorted_cover_group['Coverage group'], 'rocket', 'Weft Tear Range','STD_Weft_Tear')
-    warp_tear = box_plot(selection_df, col1, 'Warp Tear', sorted_cover_group['Coverage group'], 'winter', 'Warp Tear Range','STD_Warp_Tear')
+    with tab1:
+        tab1.subheader(f'Tear Strength')
+        col1, col2 = tab1.columns(2)
+        sorted_cover_group = selection_df.sort_values(by=["Coverage group"], ascending=False)
+        with col1:
+            warp_tear = box_plot(selection_df, 'Warp Tear', sorted_cover_group['Coverage group'], 'winter', 'Warp Tear Range','STD_Warp_Tear')
+        with col2:
+            weft_tear = box_plot(selection_df, 'Weft Tear', sorted_cover_group['Coverage group'], 'rocket', 'Weft Tear Range','STD_Weft_Tear')
 
-    tab1.subheader(f'Tensile Strength')
-    col1, col2 = tab1.columns(2)
-    weft_tensile = box_plot(selection_df, col2, 'Weft Tensile', sorted_cover_group['Coverage group'],  'rocket', 'Weft Tensile Range','STD_Weft_Tensile')
-    warp_tensile = box_plot(selection_df, col1, 'Warp Tensile', sorted_cover_group['Coverage group'],  'winter', 'Warp Tensile Range','STD_Warp_Tensile')
+        tab1.subheader(f'Tensile Strength')
+        col1, col2 = tab1.columns(2)
+        with col1:
+            warp_tensile = box_plot(selection_df, 'Warp Tensile', sorted_cover_group['Coverage group'],  'winter', 'Warp Tensile Range','STD_Warp_Tensile')
+        with col2:
+            weft_tensile = box_plot(selection_df, 'Weft Tensile', sorted_cover_group['Coverage group'],  'rocket', 'Weft Tensile Range','STD_Weft_Tensile')
 
-    tab1.subheader(f'Stretch Parameters')
-    col1, col2 = tab1.columns(2)
-    growth = box_plot(selection_df, col1, 'Growth', sorted_cover_group['Coverage group'], 'rocket', 'Growth Range', 'Growth')
-    elongation = box_plot(selection_df, col2, 'Elongation', sorted_cover_group['Coverage group'], 'winter', 'Elongation Range', 'Elongation')
+        tab1.subheader(f'Stretch Parameters')
+        col1, col2 = tab1.columns(2)
+        with col1:
+            growth = box_plot(selection_df, 'Growth', sorted_cover_group['Coverage group'], 'rocket', 'Growth Range', 'Growth')
+        with col2:
+            elongation = box_plot(selection_df, 'Elongation', sorted_cover_group['Coverage group'], 'winter', 'Elongation Range', 'Elongation')
 except ValueError:
-    tab1.subheader('< no data to display 😵 >')
-    #df_display = st.write("")
+    with tab1:
+        st.subheader('< no data to display 😵 >')
 
 #! article dataframe display
-tab2.write(f'Article Selected: {article_selectbox}')
-df_display = tab2.dataframe(selection_df)
+with tab2:
+    st.write(f'Article Selected: {article_selectbox}')
+    st.dataframe(selection_df)
