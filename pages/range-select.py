14e85790bf3bb53c4c45fcdfe346f30d3bb2f37c
Sinchan <sinchan.tex@gmail.com>
2023-04-16 23:48:55 +0530

    code refactored #5

--- pages/range-select.py
+++ pages/range-select.py
@@ -31,11 +31,9 @@ cover_df = pd.read_csv("Coverage data.csv")
 articles_df = pd.read_csv("articles.csv")
 
 #! an apt heading
-st.header("Quality Predictor")
+st.header("Article Reference")
 
 #! single column for single article select
-all_articles = articles_df['K1'].unique()
-# article_select = st.selectbox("Articles", all_articles)
 articles_df['EPI-PPI'] = articles_df['Construction'].str.extract(r'[-*/]{1}([\d]{2,3}[*][\d]{2,3})[-*\s\b]{1}')
 spin_dict = {'Carded':'K*',
             'Carded Compact': 'K.COM', 
@@ -43,35 +41,24 @@ spin_dict = {'Carded':'K*',
             'Combed Compact': 'C.COM', 
             'Vortex':'VOR',
             'Open-End':'OE'}
-col1, col2 = st.columns(2)
-with col1:
-    warp_spin_select = st.selectbox("Warp Spinning", list(spin_dict),  help="--to be updated--")
-    # st.write(spin_dict.get(warp_spin_select))
-with col2:
-    weft_spin_select = st.selectbox("Weft Spinning", list(spin_dict),  help="--to be updated--")
-    # st.write(spin_dict.get(weft_spin_select))
-selection_df = articles_df[articles_df['Warp'].str.contains(spin_dict.get(warp_spin_select)) & articles_df['Weft'].str.contains(spin_dict.get(weft_spin_select))]
-
-#! more finer selection
-wa_match = r"\d+"
-# all_warp_list = selection_df['warp'].unique()
-# all_weft_list = selection_df['weft'].unique()
 all_weaves = articles_df['Weave'].unique()
-# wa_count_search = re.search(wa_match, all_warp_list[0])
+
+#! selection criteria
 col1, col2, col3, col4, col5 = st.columns(5)
 with col1:
-    warp_count_select = st.selectbox("Warp count-comp:", selection_df['Warp'].unique())
-    # warp_count_select2 = st.selectbox("Warp count2:", all_warp_list)
-with col2:
-    weft_count_select = st.selectbox("Weft count:", selection_df['Weft'].unique())
+    warp_spin_select = st.selectbox("Warp Spinning", list(spin_dict),  help="--to be updated--")
 with col3:
-    weave_selectbox = st.selectbox("Weave select:", selection_df['Weave'].unique())
-# with col4:
-# with col5:
+    weft_spin_select = st.selectbox("Weft Spinning", list(spin_dict),  help="--to be updated--")
+selection_df = articles_df[articles_df['Warp'].str.contains(spin_dict.get(warp_spin_select)) &   articles_df['Weft'].str.contains(spin_dict.get(weft_spin_select))]
+with col2:
+    warp_count_select = st.selectbox("Warp count list", selection_df['Warp'].unique())
+with col4:
+    weft_count_select = st.selectbox("Weft count list", selection_df['Weft'].unique())
+with col5:
+    weave_selectbox = st.selectbox("Weave select", selection_df['Weave'].unique())
+# selection_df = selection_df[selection_df['Warp'].str.contains(warp_count_select) &   selection_df['Weft'].str.contains(weft_count_select)]
 
-#! metrics display
-#wa_count_display = st.metric('Warp Count',wa_count_search[0])
-#wa_disp = st.write(wa_count_search)
+
 #! dataframe display
 tab1, tab2 = st.tabs(['Selected Data', 'All Data'])
 with tab1:
