165c4e835778c3384d808508858a8b6866068aae
Sinchan <sinchan.tex@gmail.com>
2023-04-20 23:43:45 +0530

    sliders+check added

--- pages/range-select.py
+++ pages/range-select.py
@@ -44,23 +44,29 @@ spin_dict = {'Carded':'\d+K[.]',
             'Vortex':'VOR',
             'Open-End':'OE'}
 all_weaves = articles_df['Weave'].unique()
+count_list = [6, 7, 8, 10, 12, 14, 16, 20, 21, 30, 32, 40, 45, 60, 80]
 
 #! selection criteria
-col1, col2, col3, col4, col5 = st.columns(5)
+col1, col2, col3 = st.columns(3)
 with col1:
     warp_spin_select = st.selectbox("Warp Spinning", list(spin_dict),  help="Select the warp spinning technology employed")
-with col3:
-    weft_spin_select = st.selectbox("Weft Spinning", list(spin_dict),  help="Select the weft spinning technology employed")
-# st.write(str(spin_dict.get(warp_spin_select)))
-selection_df = articles_df[articles_df['Warp'].str.contains(pat = spin_dict.get(warp_spin_select), regex = True) & articles_df['Weft'].str.contains(spin_dict.get(weft_spin_select))]
+    warp_count_select = st.select_slider("Warp count", count_list, help="Select the warp count")
+    warp_ply_check = st.checkbox('Check for double ply', key=1)
+    if warp_ply_check:
+        warp_value = '2/{warp_count_select}'
+    else:
+        warp_value = warp_count_select
 with col2:
-    warp_count_select = st.selectbox("Warp counts", selection_df['Warp'].unique())
-with col4:
-    weft_count_select = st.selectbox("Weft counts", selection_df['Weft'].unique())
-with col5:
-    weave_selectbox = st.selectbox("Weave", selection_df['Weave'].unique())
-# selection_df = selection_df[selection_df['Warp'].str.contains(warp_count_select) &   selection_df['Weft'].str.contains(weft_count_select)]
-
+    weft_spin_select = st.selectbox("Weft Spinning", list(spin_dict),  help="Select the weft spinning technology employed")
+    weft_count_select = st.select_slider("Weft count", count_list, help="Select the weft count")
+    weft_ply_check = st.checkbox('Check for double ply', key=2)
+    if weft_ply_check:
+        weft_value = '2/{weft_count_select}'
+    else:
+        weft_value = weft_count_select
+with col3:
+    weave_selectbox = st.selectbox("Weave", articles_df['Weave'].unique(), help="Select the weft spinning technology employed")
+selection_df = articles_df[articles_df['Warp'].str.contains(pat = spin_dict.get(warp_spin_select)) & articles_df['Weft'].str.contains(spin_dict.get(weft_spin_select))]
 
 #! dataframe display
 tab1, tab2 = st.tabs(['Selected Data', 'All Data'])
